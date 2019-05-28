26fe50e6eb49c411b2a6c4101ce9df6de8de51b0
Scott Louvau <sclouvau@microsoft.com>
2019-05-28 11:14:00 -0700

    JsonSplitter: Queue to Array (perf)

--- src/Sarif/Map/Splitter/JsonSplitter.cs
+++ src/Sarif/Map/Splitter/JsonSplitter.cs
@@ -32,7 +32,10 @@ namespace Microsoft.CodeAnalysis.Sarif.Map.Splitter
         private BufferedReader _reader;
 
         public JsonToken Current { get; private set; }
-        private Queue<JsonToken> _decoded;
+
+        private JsonToken[] _decoded;
+        private int _decodedNextIndex;
+        private int _decodedCount;
 
         public JsonSplitter(Stream stream) : this(new BufferedReader(stream))
         { }
@@ -41,7 +44,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Map.Splitter
         {
             InitializeMap();
             _reader = reader;
-            _decoded = new Queue<JsonToken>(DecodeBatchSize);
+            _decoded = new JsonToken[DecodeBatchSize];
         }
 
         /// <summary>
@@ -81,13 +84,13 @@ namespace Microsoft.CodeAnalysis.Sarif.Map.Splitter
 
         public bool Next()
         {
-            if (_decoded.Count == 0)
+            if (_decodedNextIndex == _decodedCount)
             {
                 Split();
-                if (_decoded.Count == 0) { return false; }
+                if (_decodedCount == 0) { return false; }
             }
 
-            Current = _decoded.Dequeue();
+            Current = _decoded[_decodedNextIndex++];
             return true;
         }
 
@@ -98,12 +101,15 @@ namespace Microsoft.CodeAnalysis.Sarif.Map.Splitter
 
         private void Split()
         {
+            _decodedCount = 0;
+            _decodedNextIndex = 0;
+
             if (_reader.End) { return; }
 
             // Mark all previous tokens consumed
             if (_reader.Position > 0) { _reader.ConsumeTo(_reader.Position - 1); }
 
-            while (!_reader.End && _decoded.Count < DecodeBatchSize)
+            while (!_reader.End && _decodedCount < DecodeBatchSize)
             {
                 JsonToken token;
                 token.TokenType = JsonTokenType.EndOfFile;
@@ -144,7 +150,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Map.Splitter
                 // Mark value end
                 token.ValueEndIndex = _reader.Position - 1;
                 
-                _decoded.Enqueue(token);
+                _decoded[_decodedCount++] = token;
             }
         }
 
